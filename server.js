8313b78b5a21de5057ef879ee294ec93e13db587
unknown <suhas@legacyglobalbank.com>
2026-04-10 21:56:31 +0530

    server

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://legacy-website-popup.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
